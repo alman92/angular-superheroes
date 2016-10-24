620e9037baf9976a7d8dac6e69ea99d36173e1c4
Alex Manno <alman92@gmail.com>
2016-10-24 07:33:29 -0400

    Added more Heros

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ let superHeroesData = [
   { name: "Superman", place_of_origin: "Krypton", classification: "Hero", img_url: "http://vignette3.wikia.nocookie.net/dcanimated/images/3/39/SupermanJLU.png/revision/latest/scale-to-width-down/230?cb=20130305134005"},
   { name: "Batman", place_of_origin: "Gotham City", classification: "Hero", img_url: "http://vignette1.wikia.nocookie.net/dcanimated/images/d/d1/Batman.png/revision/latest/scale-to-width-down/230?cb=20150125193428"},
   { name: "Wonder Woman", place_of_origin: "Themescyria", classification: "Hero", img_url: "http://vignette3.wikia.nocookie.net/dcanimated/images/f/f5/Wonder_Woman.png/revision/latest/scale-to-width-down/230?cb=20090407084402"},
-  { name: "Robin", place_of_origin: "Gotham City", classification: "Sidekick", img_url: "http://vignette4.wikia.nocookie.net/dcanimated/images/8/85/RobinTNBA.png/revision/latest/scale-to-width-down/230?cb=20150111155448"}
+  { name: "Robin", place_of_origin: "Gotham City", classification: "Sidekick", img_url: "http://vignette4.wikia.nocookie.net/dcanimated/images/8/85/RobinTNBA.png/revision/latest/scale-to-width-down/230?cb=20150111155448"},
+  { name: "Wonder Girl", place_of_origin: "Themyscira", classification: "Sidekick", img_url: "http://vignette1.wikia.nocookie.net/youngjustice/images/a/a6/Wonder_Girl.png/revision/latest/scale-to-width-down/300?cb=20121015205858"},
+  { name: "Superboy", place_of_origin: "Project Cadmus", classification: "Sidekick", img_url: "http://vignette3.wikia.nocookie.net/youngjustice/images/4/47/Superboy.png/revision/latest/scale-to-width-down/300?cb=20130204001847"},
+
 ]
 
 angular
